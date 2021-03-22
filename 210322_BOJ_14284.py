fd40797faf60f439a7a45b509b865a3d5682c463
kobyunghwa <bhko0524@naver.com>
2021-03-22 23:52:23 +0900

    210322 BOJ 14284 간선 이어가기 2
    Python

---
+++ 210322_BOJ_14284.py
@@ -0,0 +1,39 @@
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for next_node, next_dist in graph[now]:
+            cost = dist + next_dist
+
+            if distance[next_node] > cost:
+                distance[next_node] = cost
+                heapq.heappush(q, (cost, next_node))
+
+
+s, t = map(int, input().split())
+
+dijkstra(s)
+
+print(distance[t])
